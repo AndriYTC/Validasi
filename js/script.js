ad95664ac2be8a0fe04a906178b29f2eb3f9cd63
AndriYTC <andriyanto28021990@gmail.com>
2024-08-13 09:48:41 +0700

    perubahan

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function validasi() {
     var email = document.forms["formku"]["email"].value;
     if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
         alert("Nama harus berupa huruf,angka,dan @");
-        document.getElementById("nama").focus();
+        document.getElementById("email").focus();
         document.formku.nama.value = "";
         return false;    
     }
